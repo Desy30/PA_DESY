8a62e28b59993978ab07871aea06fe8016c9dd2b
jrfs22 <mr.jrfs22@gmail.com>
2025-04-16 01:46:40 +0700

    update

--- app/Http/Controllers/PetaniController.php
+++ app/Http/Controllers/PetaniController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 
 class PetaniController extends Controller
 {
     public function index()
     {
-            return view('petani.index');
-        }
-
-        public function create()
-        {
-            return view('petani.create');
-        }
+        return view('admin.petani.index');
     }
+
+    public function create()
+    {
+        return view('admin.petani.create');
+    }
+
+    public function edit($id)
+    {
+        return view('admin.petani.edit');
+    }
+}
 
--- resources/views/layouting/guest/_partials/scripts.blade.php
+++ resources/views/layouting/guest/_partials/scripts.blade.php
@@ -3,13 +3,11 @@
 <script src="{{ asset('assets/guest/vendors/scripts/script.min.js') }}"></script>
 <script src="{{ asset('assets/guest/vendors/scripts/process.js') }}"></script>
 <script src="{{ asset('assets/guest/vendors/scripts/layout-settings.js') }}"></script>
-<script src="{{ asset('assets/guest/src/plugins/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('assets/guest/src/plugins/datatables/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('assets/guest/src/plugins/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('assets/guest/src/plugins/datatables/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{asset ('assets/guest/src/plugins/datatables/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/guest/plugins/datatables/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/guest/plugins/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/guest/plugins/datatables/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{asset ('assets/guest/plugins/datatables/js/responsive.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('assets/guest/vendors/scripts/dashboard.js') }}"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+<script src="{{ asset('assets/guest/vendors/scripts/datatable-setting.js') }}"></script>
 
 
